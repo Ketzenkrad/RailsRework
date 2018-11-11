04d814ca34c5a796c7ffbf2d48a950a7204df447
Ketzenkrad <ketzenkrad@gmail.com>
2018-11-12 02:02:09 +0300

    Роуты, контроллеры, оформление

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'main#index', as: 'home'
+  get 'about' => 'about#aboutme', as: 'about'
+  resources :main
 end
